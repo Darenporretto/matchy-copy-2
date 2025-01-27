a1a2799ea0d27cdb23e79f2a70be79eaa1d40a05
Darenporretto <porrettodaren@gmail.com>
2025-01-27 16:40:27 -0600

    data.js step 3

--- data.js
+++ data.js
@@ -7,11 +7,30 @@
  * See the README for detailed instructions,
  * and read every instruction carefully.
  */
-
+/*
+I: The input is an animal species. 
+O: The output will print the animal objefct using the console. 
+C: Use dot notation and bracket notation. 
+E: N/A
+*/
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// Create an empty array for your for your animal
+var animal = {}
+
+// With dot notation give the animal a species
+animal.species = 'bird';
+
+// With bracket notation give the animal a name 
+animal['name'] = 'Sam';
+
+// With dot notation give the animal a noise property starting with an empty array
+animal.noises = []
+
+// Print object to the log
+console.log(animal);
 
 
 
@@ -19,12 +38,43 @@
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// Initialize an empty array called noises
+var noises = [];
+
+// Add a noise using bracket notation
+noises[0] = 'chirp';
+
+// Add a noise using .push() method
+noises.push('squawk')
+
+// Add a noise to the front using the .unshift() method
+noises.unshift('whistle');
+
+// Add a noise to the end using bracket and length 
+noises[noises.length] = 'sing';
+
+// Print the noises array length
+console.log(noises,length);
+
+// Print last noises element without hard coding th index
+console.log(noises[noises.length -1]);
+
+// Print the whole array
+console.log(noises);
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+// Use bracket syntax and add anothe property to 'noises'
+animal['noises'] = noises;
+
+// Add another property to noise using .push() method
+animal.noises.push('flap');
+
+// Print the entire object
+console.log(animal);
 
 
 /* *******************************************************************
